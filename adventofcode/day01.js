bad3b7420310e479432cad05c79efe282bbd5f55
Stefan Maric <alexstefan92@gmail.com>
2015-12-02 19:25:36 -0430

    Add day01 and day02 solutions to AdventOfCode.com

---
+++ adventofcode/day01.js
@@ -0,0 +1,32 @@
+/*
+ * Run in: http://adventofcode.com/day/1/input
+ */
+
+;(function () {
+  let input = document.querySelector('pre').textContent
+
+  console.log(
+    'Day01/first:',
+    first(input)
+  )
+
+  console.log(
+    'Day01/second:',
+    second(input)
+  )
+
+  function first (input) {
+    return input.match(/\(/g).length - input.match(/\)/g).length
+  }
+
+  function second (input) {
+    let i = 0
+    input = input.split('')
+
+    for (let floor = 0; floor !== -1; i++) {
+      floor += (input[i] === '(') ? 1 : -1
+    }
+
+    return i
+  }
+}())
---
+++ adventofcode/day02.js
@@ -0,0 +1,46 @@
+/*
+ * Run in: http://adventofcode.com/day/2/input
+ */
+
+;(function () {
+  let input = document.querySelector('pre').textContent.slice(0, -1)
+
+  console.log(
+    'Day02/first:',
+    first(input)
+  )
+
+  console.log(
+    'Day02/second:',
+    second(input)
+  )
+
+  function first (input) {
+    return input
+      .split('\n')
+      .map(l => l.split('x').map(i => +i))
+      .map(el => {
+        return [
+          el[0] * el[1],
+          el[0] * el[2],
+          el[1] * el[2]
+        ]
+      })
+      .reduce((a, b) => {
+        return a +
+          b.reduce((a, b) => a + b) * 2 +
+          b.sort((a, b) => a > b)[0]
+      }, 0)
+  }
+
+  function second (input) {
+    return input
+      .split('\n')
+      .map(l => l.split('x').map(i => +i).sort((a, b) => a > b))
+      .reduce((a, b) => {
+        return a +
+          b.slice(0, 2).reduce((a, b) => a + b) * 2 +
+          b.reduce((a, b) => a * b)
+      }, 0)
+  }
+}())
