bc0be1ae60a850d0536afd538dc1d30662afaeda
Stefan Maric <alexstefan92@gmail.com>
2015-12-05 11:41:51 -0430

    Add day05-first solution to AdventOfCode.com

---
+++ adventofcode/day05.js
@@ -0,0 +1,26 @@
+/*
+ * Run in: http://adventofcode.com/day/5/input
+ */
+
+;(function () {
+  let input = document.querySelector('pre').textContent.slice(0, -1)
+
+  console.log(
+    'Day05/first:',
+    first(input)
+  )
+
+  function testAll (input, rules) {
+    return rules.every(regex => regex.test(input))
+  }
+
+  function first (input) {
+    let rules = [
+      /(.*[aeiou].*){3}/,
+      /(\w)\1/,
+      /^((?!(ab|cd|pq|xy)).)*$/
+    ]
+
+    return input.split('\n').filter(word => testAll(word, rules)).length
+  }
+}())
