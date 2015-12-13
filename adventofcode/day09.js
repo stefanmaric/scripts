8c145e7f8ddb77f55303b349dd076737655d3309
Stefan Maric <alexstefan92@gmail.com>
2015-12-13 13:00:48 -0430

    More use of ES2015 to day09

--- adventofcode/day09.js
+++ adventofcode/day09.js
@@ -17,7 +17,7 @@
   }
 
   function getRouteDistances (input) {
-    let extractor = /(\w+) to (\w+) = (\d+)/
+    const extractor = /(\w+) to (\w+) = (\d+)/
     let distances = input.split('\n').map(l => l.match(extractor))
 
     let destinies = Array.from(distances.reduce((a, b) => {
@@ -27,27 +27,27 @@
     let permutations = permute(destinies)
 
     distances = distances.reduce((a, b) => {
-      a[b[1] + ',' + b[2]] = +b[3]
+      a[`${b[1]},${b[2]}`] = +b[3]
       return a
     }, {})
 
     return permutations.map(el => {
       return el.reduce((a, b, i, arr) => {
         return a + (
-          distances[b + ',' + arr[i + 1]] ||
-          distances[arr[i + 1] + ',' + b] ||
+          distances[`${b},${arr[i + 1]}`] ||
+          distances[`${arr[i + 1]},${b}`] ||
           0)
       }, 0)
     }).sort((a, b) => a > b)
   }
 
   function permute (arr) {
-    return arr.length <= 2
-      ? [ arr.slice(), arr.slice().reverse() ]
-      : arr.map((el, i, arr) => {
+    return arr.length - 2 &&
+      [].concat(...arr.map((el, i, arr) => {
         let cp = arr.slice()
         let cu = cp.splice(i, 1)
-        return permute(cp).map(el => el.concat(cu))
-      }).reduce((a, b) => a.concat(b), [])
+        return permute(cp).map(el => cu.concat(el))
+      })) ||
+      [ arr.slice(), arr.slice().reverse() ]
   }
 }())
