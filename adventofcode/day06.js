c445dc23638fa977be974b2277d3c1a971a2f1c2
Stefan Maric <alexstefan92@gmail.com>
2015-12-13 12:53:09 -0430

    DRY up day06

--- adventofcode/day06.js
+++ adventofcode/day06.js
@@ -24,48 +24,34 @@
   }
 
   function first (input) {
-    return input.split('\n')
-      .map(l => l.match(/(on|off|toggle) (\d+,\d+) through (\d+,\d+)/).slice(1))
-      .reduce((matrix, inst) => {
-        let fillWith
-
-        switch (inst[0]) {
-          case 'on':
-            fillWith = 1
-            break
-          case 'off':
-            fillWith = 0
-            break
-          case 'toggle':
-            fillWith = (val) => val ? 0 : 1
-            break
-        }
-
-        return fillMatrix(matrix, fillWith, inst[1], inst[2])
-      }, Array(1000).fill().map(() => Array(1000).fill(0)))
+    const map = {
+      'on': 1,
+      'off': 0,
+      'toggle': val => val ? 0 : 1
+    }
+    return animateMatrix(input, map)
       .reduce((count, row) => count + row.filter(el => el).length, 0)
   }
 
   function second (input) {
-    return input.split('\n')
-      .map(l => l.match(/(on|off|toggle) (\d+,\d+) through (\d+,\d+)/).slice(1))
-      .reduce((matrix, inst) => {
-        let fillWith
-
-        switch (inst[0]) {
-          case 'on':
-            fillWith = (val) => ++val
-            break
-          case 'off':
-            fillWith = (val) => val ? --val : 0
-            break
-          case 'toggle':
-            fillWith = (val) => val + 2
-            break
-        }
-
-        return fillMatrix(matrix, fillWith, inst[1], inst[2])
-      }, Array(1000).fill().map(() => Array(1000).fill(0)))
+    const map = {
+      'on': val => ++val,
+      'off': val => val ? --val : 0,
+      'toggle': val => val + 2
+    }
+    return animateMatrix(input, map)
       .reduce((count, row) => count + row.reduce((a, b) => a + b), 0)
+  }
+
+  function getInstructions (input) {
+    return input
+      .split('\n')
+      .map(l => l.match(/(on|off|toggle) (\d+,\d+) through (\d+,\d+)/).slice(1))
+  }
+
+  function animateMatrix (input, map) {
+    return getInstructions(input)
+      .reduce((m, inst) => fillMatrix(m, map[inst[0]], inst[1], inst[2]),
+        Array(1000).fill().map(() => Array(1000).fill(0)))
   }
 }())
