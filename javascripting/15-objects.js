fbebe128956210de3db9ba2cd0d1443a72737dbb
Stefan Maric <alexstefan92@gmail.com>
2016-12-08 20:57:08 -0400

    Import my solution to javascripting's OBJECTS

---
+++ javascripting/15-objects.js
@@ -0,0 +1,15 @@
+;(function () {
+  'use strict'
+
+  let pizza = {
+    toppings: [
+      'cheese',
+      'sauce',
+      'pepperoni'
+    ],
+    crust: 'deep dish',
+    serves: 2
+  }
+
+  console.log(pizza)
+})()
