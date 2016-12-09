f1f4b3a8733daed6e5f6180faae6e55a5b89c468
Stefan Maric <alexstefan92@gmail.com>
2016-12-08 20:57:08 -0400

    Import my solution to javascripting's IF STATEMENT

---
+++ javascripting/09-if-statement.js
@@ -0,0 +1,14 @@
+;(function () {
+  'use strict'
+
+  let fruit = 'orange'
+  let message
+
+  if (fruit.length > 5) {
+    message = 'The fruit name has more than five characters.'
+  } else {
+    message = 'The fruit name has five characters or less.'
+  }
+
+  console.log(message)
+})()
